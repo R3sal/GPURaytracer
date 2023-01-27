adbf2b95885e6e79cd5160e02084e55e5f732d00
R3sal <103441184+R3sal@users.noreply.github.com>
2023-01-27 09:07:06 +0100

    updated default scene

--- src/Settings.h
+++ src/Settings.h
@@ -5,9 +5,9 @@
 #define RT_WINDOW_HEIGHT 720
 
 //raytracing properties
-#define RT_SCENE_FILENAME "assets/testscene2.obj"
+#define RT_SCENE_FILENAME "assets/testscene1.obj"
 #define RT_MAX_RAYS_PER_PIXEL 1; //number of rays per pixel, the higher this value, the better AA and DOF effects will be
-#define RT_MAX_RAY_DEPTH 4; //after shooting this number of rays, we return to shooting a ray from the camera
+#define RT_MAX_RAY_DEPTH 3; //after shooting this number of rays, we return to shooting a ray from the camera
 #define RT_AA_SAMPLE_SPREAD 1.5f; //anti-aliasing: the bigger the value, the blurrier the image, disabled at 0.0f, default is 1.0f
 #define RT_DOF_SAMPLE_SPREAD 0.0f; //depth of field: the bigger the value, the stronger the DOF effect, disabled at 0.0f, default is 1.0f
 #define RT_USE_BVH 1 //determines the usage of a bounding volume hierarchy (0: do not use BVH, 1: use BVH)
@@ -20,14 +20,14 @@
 #define RT_CAMERA_FARZ 10000.0f //objects that are farther away from the camera than this, are not rendered
 
 //for testscene2.obj
-#define RT_CAMERA_POSITION { 0.0f, 0.0f, 0.0f }; //the position of the camera
-#define RT_CAMERA_FOCUS_POINT { 1.0f, 0.0f, 0.0f }; //the point, the camera will look at
-#define RT_CAMERA_UP_DIRECTION { 0.0f, 1.0f, 0.0f }; //adjusts, if the camera leans in a certain direction
+//#define RT_CAMERA_POSITION { 0.0f, 0.0f, 0.0f }; //the position of the camera
+//#define RT_CAMERA_FOCUS_POINT { 1.0f, 0.0f, 0.0f }; //the point, the camera will look at
+//#define RT_CAMERA_UP_DIRECTION { 0.0f, 1.0f, 0.0f }; //adjusts, if the camera leans in a certain direction
 
 //for testscene1.obj
-//#define RT_CAMERA_POSITION { -4.0259f, 2.1676f, -2.9459f }; //the position of the camera
-//#define RT_CAMERA_FOCUS_POINT { 0.0f, 0.0f, 0.0f }; //the point, the camera will look at
-//#define RT_CAMERA_UP_DIRECTION { 0.0f, 1.0f, 0.0f }; //adjusts, if the camera leans in a certain direction
+#define RT_CAMERA_POSITION { -4.0259f, 2.1676f, -2.9459f }; //the position of the camera
+#define RT_CAMERA_FOCUS_POINT { 0.0f, 0.0f, 0.0f }; //the point, the camera will look at
+#define RT_CAMERA_UP_DIRECTION { 0.0f, 1.0f, 0.0f }; //adjusts, if the camera leans in a certain direction
 
 //for testobject4_2.obj
 //#define RT_CAMERA_POSITION { -2.5f, 5.0f, -2.0f }; //the position of the camera
